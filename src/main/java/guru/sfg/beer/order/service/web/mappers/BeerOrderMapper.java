4a0984e6f9bb0ae242f36f5452c91ededad5dd76
TairovichJR <yokubov.omonjon@gmail.com>
2021-03-26 00:21:55 -0400

    initial commit

---
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
@@ -0,0 +1,13 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.web.model.BeerOrderDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
+public interface BeerOrderMapper {
+
+    BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
+
+    BeerOrder dtoToBeerOrder(BeerOrderDto dto);
+}
